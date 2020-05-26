f05902836674a4e9999eb76fd1b2efaf33d9718c
vortechron <sayaamiruladli@gmail.com>
2020-05-27 00:29:15 +0800

    wip

--- src/components/tabnav/tabNavButton.vue
+++ src/components/tabnav/tabNavButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :href="(!isActive) ? url : null" class="border-gray-200 cursor-pointer" :class="[isActive ? 'border-b-4 border-indigo-700' : 'bg-gray-100' ]">
+    <a :href="(!isActive) ? url : null" class="border-gray-200 cursor-pointer" :class="[isActive ? 'border-b-4 border-indigo-700 bg-white' : 'bg-gray-100' ]">
         <div class="px-4 py-5 sm:pt-4 sm:pb-2">
             <mini-heading :icon="icon" :title="title" is-center :class="[isActive ? 'text-indigo-700' : '' ]"></mini-heading>
         </div>
